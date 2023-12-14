fd31c8875197f8f2eff534afb870b9786989aac2
Lexi303 <dankovicki@gmail.com>
2023-12-14 12:15:08 +0100

    Conditions

---
+++ script/script.js
@@ -0,0 +1,18 @@
+function checkTime() {
+    // Få gjeldende klokkeslett
+    const currentHour = new Date().getHours();
+
+    // Betingelser for å gi respons basert på klokkeslett
+    let greeting;
+    if (currentHour >= 5 && currentHour < 12) {
+        greeting = 'God morning';
+    } else if (currentHour >= 12 && currentHour < 18) {
+        greeting = 'God dag';
+    } else {
+        greeting = 'God kveld';
+    }
+
+    // Vis responsen i konsollen
+    console.log(greeting);
+
+}
